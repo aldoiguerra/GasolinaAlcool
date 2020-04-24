4a017d752913a3e813786f665ba2481c2f6efc50
Aldo Ianelo Guerra <aldoiguerra@gmail.com>
2020-04-24 00:41:13 -0300

    aula finalizada

--- GasolinaAlcool/ViewController.swift
+++ GasolinaAlcool/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var precoEtanol: UITextField!
+  @IBOutlet weak var precoGasolina: UITextField!
+  @IBOutlet weak var resultado: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func calcular(_ sender: Any) {
+    
+    let etanol = ((precoEtanol.text ?? "0") as NSString).doubleValue
+    let gasolina = ((precoGasolina.text ?? "0") as NSString).doubleValue
+    
+    
+    
+    let proporcao = etanol / gasolina;
+    
+    if(proporcao < 0.7){
+      
+      resultado.text = "Compensa o Etanol \(proporcao)."
+      
+    }else{
+      
+      resultado.text = "Compensa a Gasolina \(proporcao)."
+      
+    }
+    
+    
+  }
+  
 }
 
