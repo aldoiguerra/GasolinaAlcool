80289b7eecb3fb8e198de0b47304e77ae9ff3d92
Aldo Ianelo Guerra <aldoiguerra@gmail.com>
2020-04-23 23:56:00 -0300

    Initial Commit

---
+++ GasolinaAlcool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GasolinaAlcool
+//
+//  Created by Aldo Ianelo Guerra on 23/04/20.
+//  Copyright © 2020 Aldo Ianelo Guerra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
